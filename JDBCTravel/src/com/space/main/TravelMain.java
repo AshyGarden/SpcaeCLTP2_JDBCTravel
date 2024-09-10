d5c3e1a3f635c0de0a5ee1f413f756c17f80d4b9
seongjin <frjo1078@gmail.com>
2024-09-10 09:41:22 +0900

    깃허브 테스트 입니다.

--- JDBCTravel/src/com/space/main/TravelMain.java
+++ JDBCTravel/src/com/space/main/TravelMain.java
@@ -6,7 +6,9 @@ import com.space.global.AppUI;
 public class TravelMain {
 	
 	public static void main(String[] args) {	
-
+		System.out.println("test");
+		
+		
 		AppController controller = new AppController();
 		
 		while(true) {
