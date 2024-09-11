aa5422fd6baf838bbff2211df285b37fbd08232e
jun940317 <jun940317@gmail.com>
2024-09-11 12:35:15 +0900

    240911 commit TravelSearchService

--- JDBCTravel/src/com/space/service/search/TravelSearchService.java
+++ JDBCTravel/src/com/space/service/search/TravelSearchService.java
@@ -1,5 +1,7 @@
-package com.space.service;
+package com.space.service.search;
 
+import com.space.dao.functiondao.JDBCTravelDAO;
+import com.space.dao.interfacedao.TravelDAO;
 import com.space.global.*;
 
 public class TravelSearchService implements Start {
@@ -13,19 +15,19 @@ public class TravelSearchService implements Start {
             switch (selection) {
                 case GlobalParams.returnNum:                //상위 메뉴로 돌아가기
                     return; //메인화면으로 돌아가기
-                case GlobalParams.searchRsv:        //예약 삭제
+                case GlobalParams.searchRsv:                //예약 조회
                     SearchReservation();
                     break;
-                case GlobalParams.searchTravelPackage:      //여행패키지 추가
+                case GlobalParams.searchTravelPackage:      //여행패키지 조회
                     SearchTravel();
                     break;
-                case GlobalParams.searchTravelPlace:        //여행지 추가
+                case GlobalParams.searchTravelPlace:        //여행지 조회
                     SearchPlace();
                     break;
-                case GlobalParams.searchTravelLodging:      //숙박시설 추가
+                case GlobalParams.searchTravelLodging:      //숙박시설 조회
                     SearchLodging();
                     break;
-                case GlobalParams.searchTravelFood:         //음식 추가
+                case GlobalParams.searchTravelFood:         //음식 조회
                     SearchFood();
                     break;
                 default:
@@ -49,7 +51,8 @@ public class TravelSearchService implements Start {
     }
 
     private void SearchTravel() {
-
+        TravelDAO dao = new JDBCTravelDAO();
+        dao.findAllTravels();
     }
 
     private void SearchReservation() {
