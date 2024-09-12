fb889974a36bc971b6d1e8883aa2c04966b5e890
youngungit <jrmrahn21@gmail.com>
2024-09-12 16:37:19 +0900

    final change

--- JDBCTravel/src/com/space/dao/interfacedao/TravelDAO.java
+++ JDBCTravel/src/com/space/dao/interfacedao/TravelDAO.java
@@ -2,6 +2,6 @@ package com.space.dao.interfacedao;
 
 public interface TravelDAO {
     void deleteTravelByNO(int travelNumber);
-    boolean updateTravelByNo(int travelNumber);
+    void updateTravelByNo(int travelNumber);
     void insertTravel();
 }
