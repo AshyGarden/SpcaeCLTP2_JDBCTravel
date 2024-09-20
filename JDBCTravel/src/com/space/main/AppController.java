ebdabd024c634930230565f62520b8df2bfb6006
jun940317 <jun940317@gmail.com>
2024-09-20 09:35:03 +0900

    240920 AppCon Edit

--- JDBCTravel/src/com/space/main/AppController.java
+++ JDBCTravel/src/com/space/main/AppController.java
@@ -5,25 +5,28 @@ import com.space.global.*;
 import com.space.service.TravelInsertService;
 import com.space.service.TravelUpdateService;
 import com.space.service.TravelDeleteService;
+import com.space.service.search.TravelSearchService;
 
 public class AppController {
-	
+
+	TravelInsertService travelInsertService = new TravelInsertService();
+	TravelSearchService travelSearchService = new TravelSearchService();
 	TravelUpdateService travelUpdateService = new TravelUpdateService();
+	TravelDeleteService travelDeleteService = new TravelDeleteService();
 	
-	public void chooseSystem(int inputNum) {		
-		switch(inputNum) {  	
-	    	//여행 및 패키지 추가 - 성진
-	    	case GlobalParams.travelInsertNo:
-	    		System.out.println("여행 및 패키지 추가 모드로 돌입합니다.");
-	    		//tCancel.start();
-				TravelInsertService travelInsertService = new TravelInsertService();
+	public void chooseSystem(int inputNum) {
+
+		switch(inputNum) {
+			//여행 및 패키지 추가 - 성진
+			case GlobalParams.travelInsertNo:
+				System.out.println("여행 및 패키지 추가 모드로 돌입합니다.");
 				travelInsertService.start();
 	    		break;
 	    		
 	    	//여행 및 패키지 조회 - 용준
 	    	case GlobalParams.travelSearchNo:
 	    		System.out.println("여행 및 패키지 조회 모드로 돌입합니다.");
-	    		//tCancel.start();
+				travelSearchService.start();
 	    		break;
 	    		
 	    	//여행 및 패키지 수정 - 재연
@@ -35,16 +38,14 @@ public class AppController {
 	    	//여행 및 패키지 취소 - 영훈
 	    	case GlobalParams.travelDeleteNo:
 	    		System.out.println("여행 및 패키지 취소 모드로 돌입합니다.");
-	    		//tCancel.start();
-				TravelDeleteService travelDeleteService = new TravelDeleteService();
-				travelDeleteService.start();
+	    		travelDeleteService.start();
 	    		break;
 	    		
 	    	//여행 및 패키지 기록 (예정)
-	    	case GlobalParams.travelHistoryNo:
-	    		System.out.println("여행 및 패키지 기록 모드로 돌입합니다.");
-	    		//tSuggest.start();
-	    		break;
+//	    	case GlobalParams.travelHistoryNo:
+//	    		System.out.println("여행 및 패키지 기록 모드로 돌입합니다.");
+//	    		//tSuggest.start();
+//	    		break;
 	    	
 	    	//프로그램 종료
 	    	case GlobalParams.terminateNum:
